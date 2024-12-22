67da46ce5a145fb50a9a3149c16f84663e1443d4
Meet Sundrani <meetsundrani@gmail.com>
2024-12-22 20:30:02 +0530

    updated cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/meet_sundrani_cv.pdf";
+import pdf from "../../Assets/../Assets/meetsundrani_cv_2.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/meetsundrani/portfolio/master/src/Assets/meet_sundrani_cv.pdf";
+  "https://raw.githubusercontent.com/meetsundrani/portfolio/master/src/Assets/meetsundrani_cv_2.pdf";
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
